df631bfcbe25ac3b1035c86c22f008414d8ebf64
hbdgr <hbdgr@protonmail.com>
2017-10-16 13:59:32 +0200

    py-scripts move check_succesful_tx to utils

--- scripts/DToken_transact.py
+++ scripts/DToken_transact.py
@@ -2,24 +2,8 @@ import populus
 from populus.utils.wait import wait_for_transaction_receipt
 from web3 import Web3
 
+import utils
 
-"""
-Inspiration from official populus documentation.
-http://populus.readthedocs.io
-"""
-def check_succesful_tx(web3: Web3, txid: str, timeout=600) -> dict:
-    """See if transaction went through (Solidity code did not throw).
-
-    :return: Transaction receipt
-    """
-
-    # http://ethereum.stackexchange.com/q/6007/620
-    receipt = wait_for_transaction_receipt(web3, txid, timeout=timeout)
-    txinfo = web3.eth.getTransaction(txid)
-
-    # EVM has only one error mode and it's consume all gas
-    assert txinfo["gas"] != receipt["gasUsed"]
-    return receipt
 
 
 def performTransfer():
@@ -52,7 +36,7 @@ def performTransfer():
 
         print("Transaction hash: {}".format(txhash))
 
-        receipt = check_succesful_tx(web3, txhash)
+        receipt = utils.check_succesful_tx(web3, txhash)
         print("Transaction was confirmed")
 
 
