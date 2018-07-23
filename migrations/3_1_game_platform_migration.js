f4743a388d04b578589e6fa178c3d3dbbd841df4
hbdgr <hbdgr@protonmail.com>
2018-07-23 11:13:04 +0200

    different migratios for standalone PlatformDeposit and GameDeposit

--- migrations/3_1_game_platform_migration.js
+++ migrations/3_1_game_platform_migration.js
@@ -1,14 +1,14 @@
 const config = require('../config.json');
 
 const JoyToken = artifacts.require('JoyToken');
-const PlatformDeposit = artifacts.require('PlatformDeposit');
+const GameDeposit = artifacts.require('GameDeposit');
 const JoyGameDemo = artifacts.require('JoyGameDemo');
 
 
 module.exports = (deployer) => {
 	deployer.deploy(JoyToken, { overwrite: false })
 		.then(() =>
-			deployer.deploy(PlatformDeposit, JoyToken.address, config.platformReserve))
+			deployer.deploy(GameDeposit, JoyToken.address, config.platformReserve))
 		.then(() =>
-			deployer.deploy(JoyGameDemo, PlatformDeposit.address, config.gameDeveloper));
+			deployer.deploy(JoyGameDemo, GameDeposit.address, config.gameDeveloper));
 };
